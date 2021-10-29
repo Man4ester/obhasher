f51bab65c738a70abafa0362169408841277b5c2
Oleksandr Burkovskyi <oleksandr_burkovskyi@epam.com>
2021-10-29 22:28:17 +0300

    init hasher module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Man4ester/obhasher
+
+go 1.17
---
+++ obhasher.go
@@ -0,0 +1,12 @@
+// Package obhasher is a module for hashing txt valuse and validation
+package obhasher
+
+// HashPassword function for hashing txt
+func HashPassword(password string) (string, error) {
+	return "", nil
+}
+
+// CheckPasswordHash function for checking hashed txt via hash
+func CheckPasswordHash(password, hash string) bool {
+	return true
+}
